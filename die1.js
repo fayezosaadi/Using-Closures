25ba3d54deed866622a0ed835a9cfdcf809c1897
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-07 03:14:27 +0000

    Countdown

--- die1.js
+++ die1.js
@@ -1,19 +1,20 @@
 
-function makeLoadedDie() {
+  function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
   /* your code here */
-var i=0;
+  var i=0;
+
   return function() {
     /* your code here */
 
     return list[i++]
 
   }
-}
+  }
 
-var rollLoadedDie = makeLoadedDie();
+  var rollLoadedDie = makeLoadedDie();
 
-console.log(rollLoadedDie());  // 5
-console.log(rollLoadedDie());  // 4
-console.log(rollLoadedDie());  // 6
-console.log(rollLoadedDie());  // 6
+  console.log(rollLoadedDie());  // 5
+  console.log(rollLoadedDie());  // 4
+  console.log(rollLoadedDie());  // 6
+  console.log(rollLoadedDie());  // 6
